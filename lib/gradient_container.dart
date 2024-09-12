c23a6c5998bea115bc487404ba76ef63406ea69b
Parth Vasvani <parthvasvani@parths-macbook-air.local>
2024-09-12 19:58:43 +0530

    SecondDay-Shape

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -13,15 +13,76 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: colors,
-            begin: startAlignment,
-            end: endAlignment,
-          )),
-      child: const Center(
-        child: StyleText("PARTH"),
-      ),
-    );
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        )),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 14, 81, 137),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(40)),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/food1.jpg"),
+                    ),
+                    border: Border(
+                        right: BorderSide(color: Colors.green, width: 10))),
+                child: const Center(
+                  child: Text(
+                    "data",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blue[500], shape: BoxShape.circle),
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Image.asset("assets/images/food1.jpg"),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // 3
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blue[500],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.elliptical(400, 800),
+                        topRight: Radius.elliptical(400, 800))),
+                child: const StyleText("data"),
+              ),
+              // 4
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepPurple,
+                child: const StyleText("Dev"),
+              )
+            ],
+          ),
+        ));
   }
 }
